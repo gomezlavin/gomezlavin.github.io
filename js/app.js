767d5b973ed2159e05c5c47ee0f0cd05a6ea5086
Santiago Gomez Lavin <gomezlavin@gmail.com>
2014-01-21 16:29:00 -0800

    change in resume

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 
 $(document).ready(function(){
   $('nav').localScroll({duration:700});
+
+  causeRepaintsOn = $("h1, h2, h3, p");
+
+  $(window).resize(function() {
+    causeRepaintsOn.css("z-index", 1);
+  });
 });
